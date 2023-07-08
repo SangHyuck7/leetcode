f174f1acf41bc9af78cd72ef2add0ca476be9b35
Sang Hyuk <107290583+HyukE@users.noreply.github.com>
2023-07-08 21:57:19 +0900

    Time: 71 ms (65.71%), Space: 43.9 MB (88.52%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+//시간 복잡도: O(n)
+const insert = function (intervals, newInterval) {
+  let [start, end] = newInterval;
+  const left = [];
+  const right = [];
+  
+  for (const interval of intervals) {
+    const [first, last] = interval;
+	
+    if (last < start) left.push(interval);
+	
+    else if (first > end) right.push(interval);
+	
+    else {
+      start = Math.min(start, first);
+      end = Math.max(end, last);
+    }
+  }
+  
+  return [...left, [start, end], ...right]; 
+};
