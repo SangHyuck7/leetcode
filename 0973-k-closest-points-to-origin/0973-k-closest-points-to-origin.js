6d62dc327e016b806c49e0afe9497274386e8c25
Sang Hyuk <107290583+HyukE@users.noreply.github.com>
2023-07-11 15:05:24 +0900

    Time: 224 ms (60.61%), Space: 61.4 MB (54.77%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+const dist = (point) => {
+  return point[0] * point[0] + point[1] * point[1];
+}
+
+const kClosest = function(points, k) {
+  const n = points.length;
+  let map = [];
+
+  for (let i = 0; i < n; ++i) {
+    map.push({
+      distance: dist(points[i]),
+      points: points[i]
+    })
+  }
+  map.sort((a, b) => a.distance - b.distance);
+    
+  let ans = [];
+  for (let j = 0; j < k; j++) {
+      ans.push(map[j].points)
+  }
+  return ans;
+};
