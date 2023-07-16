6b6e5eda59c8297651758a4e1be16e56be9f9a00
Sang Hyuk <107290583+HyukE@users.noreply.github.com>
2023-07-16 18:16:39 +0900

    Time: 80 ms (52.73%), Space: 45.6 MB (45.27%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+
+//시간복잡도 :O(n)
+const levelOrder = function(root) {
+  const results = [];
+    
+  if (!root) return results;
+    
+  const dfs = (node, level) => {
+      results[level] = [...(results[level] || []), node.val];
+      if (node.left) dfs(node.left, level + 1);
+      if (node.right) dfs(node.right, level + 1);
+  }
+    
+  dfs(root, 0);
+    
+  return results;
+};
