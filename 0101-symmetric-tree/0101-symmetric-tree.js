6bc0a829cfad80b34c121ef8e70298e3e0c91f91
Sang Hyuk <107290583+HyukE@users.noreply.github.com>
2023-07-22 15:23:23 +0900

    Time: 63 ms (84.61%), Space: 44.4 MB (59.07%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+const isSymmetric = function(root) {
+    return isMirror(root, root);
+};
+
+function isMirror(node1, node2) {
+  if (node1 == null && node2 == null) {
+    return true;
+  }
+  
+  if (node1 == null || node2 == null) {
+    return false;
+  }
+
+  if (node1.val == node2.val) {
+    return isMirror(node1.right, node2.left) && isMirror(node1.left, node2.right);
+  } else {
+    return false;
+  }
+}
