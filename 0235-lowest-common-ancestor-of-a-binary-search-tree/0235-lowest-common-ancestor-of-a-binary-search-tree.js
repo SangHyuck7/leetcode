72f67344c55f3fb941c84ec3b699a674f4804818
이상혁 | SangHyuk Lee <107290583+HyukE@users.noreply.github.com>
2023-08-20 21:13:37 +0900

    Time: 76 ms (66.98%), Space: 52.2 MB (63.96%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -12,19 +12,15 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
-
-//이진탐색
-//시간복잡도 최악의 경우 O(N), 평균적으로는 O(log N)
-
 const lowestCommonAncestor = function(root, p, q) {
-  while (root) {
-    if (root.val < p.val && root.val < q.val) {
-      root = root.right;
-    }
-    else if (root.val > p.val && root.val > q.val) {
-    root = root.left;
-    } else {
-      return root;
-    }
-  }
+   while(root){
+       if (p.val < root.val && q.val < root.val) {
+           root = root.left;
+       } else if (p.val > root.val && q.val > root.val) {
+           root = root.right;
+       } else {
+         break;
+       }
+   }
+    return root;
 };
