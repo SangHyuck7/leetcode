90b7da96b3b43abb72a619c3b1eaca5f3d18706f
SangHyuk <107290583+SangHyuk7@users.noreply.github.com>
2024-03-19 13:37:12 +0900

    Time: 59 ms (13.09%), Space: 48.9 MB (17.00%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.js
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -1,39 +1,19 @@
-/**
- * Definition for isBadVersion()
- * 
- * @param {integer} version number
- * @return {boolean} whether the version is bad
- * isBadVersion = function(version) {
- *     ...
- * };
- */
-
-/**
- * @param {function} isBadVersion()
- * @return {function}
- */
-
-//BINARY SEARCH
-
 const solution = function(isBadVersion) {
-    /**
-     * @param {integer} n Total versions
-     * @return {integer} The first bad version
-     */
     return function(n) {
+        let min = null;
         let start = 1;
         let end = n;
-
-        while (start <= end) {
-            let mid = Math.floor((start + end) / 2);
-
-            if (!isBadVersion(mid)) {
-                start = mid + 1;
-            } else {
+        
+        while(start <= end) {
+            const mid = Math.floor((start + end) / 2);
+            
+            if(isBadVersion(mid)) {
+                min = mid;
                 end = mid - 1;
+            } else {
+                start = mid + 1;
             }
         }
-
-        return start
+        return min;
     };
 };
