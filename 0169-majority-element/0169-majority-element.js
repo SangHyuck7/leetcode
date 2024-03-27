2c5a4aa25c22bb686e68d5bdd11d6d9857b3ce21
SangHyuk <107290583+SangHyuk7@users.noreply.github.com>
2024-03-27 10:04:17 +0900

    Time: 68 ms (22.32%), Space: 53 MB (24.63%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,13 +3,21 @@
  * @return {number}
  */
 
-//시간복잡도 O(n)
-const majorityElement = function(nums) {
-  const map = new Map();
-    
-  for (let i = 0; i < nums.length; i++) {
-    map.set(nums[i], map.has(nums[i]) ? map.get(nums[i]) + 1 : 1);
-    
-    if (map.get(nums[i]) > (nums.length / 2)) return nums[i]
+/*
+* 해쉬태이블 자료 구조 사용
+* 저장하는 과정 시간복잡도: O(n)
+* 해당 값을 가지고 오는 시간 복잡도: O(n)
+* 해당 알고리즘의 시간 복잡도: O(n)
+*/
+
+const majorityElement = (nums) => {
+  const map = {};
+
+  for (let n of nums) {
+    map[n] = (map[n] || 0) + 1;
+
+    if (map[n] > nums.length / 2) {
+      return n;
+    }
   }
 };
