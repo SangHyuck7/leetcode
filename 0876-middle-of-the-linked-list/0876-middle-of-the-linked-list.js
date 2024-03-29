07e2178e9aa9b27f794ad657b8ee97ba8c16e98a
SangHyuk <107290583+SangHyuk7@users.noreply.github.com>
2024-03-29 16:48:53 +0900

    Time: 50 ms (64.25%), Space: 48.7 MB (61.48%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -9,17 +9,39 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-//시간 복잡도 O(n)
+// 처음 해쉬 테이블 방법으로 접금 하지만 정수 반환
+// 투포인터로 문제 해결
+// 시간 복잡도는 모두 O(n)으로 동일
+// 공간 복잡도는 해쉬 테이블은 모든 노드 저장 O(n) 투포인터는 하나의 노드 저장 O(1) 
+
 const middleNode = function(head) {
-  let obj = {};
-  let current = head;
-  let counter = 1;
+  let slow = head;
+  let fast = head;
 
-  while(current) {
-    obj[counter] = current;
-    counter++;
-    current = current.next;
-    }
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
 
-    return obj[Math.ceil(counter/2)]
+  return slow;
 };
+
+// const middleNode = function(head) {
+//   const table = new Map();
+//   let count = 1;
+//   let result = null;
+
+//   while (head.val && head.next) {
+//     head = head.next;
+//     table.set(count, head.val);
+//     count += 1;
+//   }
+  
+//   if (count % 2 === 0) {
+//     result = table.get((count / 2) + 1);
+//   } else {
+//     result = table.get(Math.round(count / 2));
+//   }
+
+//   return result;
+// };
