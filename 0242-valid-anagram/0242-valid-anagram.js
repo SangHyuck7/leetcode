9cb39458eae83dbbc9f6d2b5c57824ea8cca047e
SangHyuk <107290583+SangHyuk7@users.noreply.github.com>
2024-03-31 21:01:28 +0900

    Time: 91 ms (10.78%), Space: 53.6 MB (46.16%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,25 +3,13 @@
  * @param {string} t
  * @return {boolean}
  */
-//시간 복작도 O(n)
 const isAnagram = function(s, t) {
-  if (s.length !== t.length) {
-    return false;
-  }
-    
-  const count = new Array(26).fill(0);
+  const sResult = s.split("").sort().join("");
+  const tResult = t.split("").sort().join("");
 
-  for (let i = 0; i < s.length; i++) {
-    count[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
-    count[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+  if (sResult === tResult) {
+    return true;
   }
 
-  for (let i = 0; i < 26; i++) {
-    if (count[i] !== 0) {
-        return false;
-    }
-  }
-
-  return true;
-
+  return false;
 };
