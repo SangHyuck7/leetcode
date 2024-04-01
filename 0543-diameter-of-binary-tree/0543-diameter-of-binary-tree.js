46837958645771a42d23eda36ad95ac9aaa71f58
SangHyuk <107290583+SangHyuk7@users.noreply.github.com>
2024-04-01 10:39:23 +0900

    Time: 54 ms (91.35%), Space: 58.4 MB (5.79%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
@@ -10,23 +10,25 @@
  * @param {TreeNode} root
  * @return {number}
  */
- //이해 부족 다시 풀어보기
-// 시간복잡도 O(n)
+// dfs 알고리즘 사용 
+// 시간 복잡도: O(n) 모드 노드 재귀로 방문
 const diameterOfBinaryTree = function(root) {
   let diameter = 0;
-  
-  dfs(root);
-    
-  return diameter;
-    
-  function dfs(node) {
-    if (!node) return 0;
-        
-    const left = dfs(node.left);
-    const right = dfs(node.right);
-        
-    diameter = Math.max(diameter, left + right);
 
-    return 1 + Math.max(left, right);
+  const treeDfs = (node) => {
+    if (!node) {
+      return 0;
+    }
+
+    const leftDepth = treeDfs(node.left);
+    const rightDepth = treeDfs(node.right);
+
+    diameter = Math.max(diameter, leftDepth + rightDepth);
+
+    return Math.max(leftDepth, rightDepth) + 1; // 마지막 재귀 Math.max(null, null) + 1 은 1반환
   }
+  
+  treeDfs(root);
+  
+  return diameter;
 };
