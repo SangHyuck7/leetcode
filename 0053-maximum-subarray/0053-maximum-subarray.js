945d7952368b660d15a6f2a5c6af35faa21a75a6
SangHyuk <107290583+SangHyuk7@users.noreply.github.com>
2024-04-03 10:28:06 +0900

    Time: 69 ms (70.79%), Space: 58.1 MB (27.55%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -2,21 +2,16 @@
  * @param {number[]} nums
  * @return {number}
  */
-//카데인 알고리즘
-//시간복잡도: O(n) 
+
+// 시간복잡도: O(n)
 const maxSubArray = function(nums) {
-  let max = -Infinity;
-  let meh = 0;
+  let maxEnding = nums[0]
+  let maxSum = nums[0]
 
-    for (let i = 0; i < nums.length; i++) {
-        meh += nums[i];
-        if (meh > max) {
-            max = meh;
-        }
-        if (meh < 0) {
-            meh = 0;
-        }
-    }
+  for (let i = 1; i < nums.length; i++) {
+    maxEnding = Math.max(maxEnding + nums[i], nums[i]);
+    maxSum = Math.max(maxSum, maxEnding);
+  }
 
-    return max;
-};
+  return maxSum;
+}
