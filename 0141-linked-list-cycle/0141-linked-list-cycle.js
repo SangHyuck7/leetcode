db5756bdb2d2c649cffe673019a367a437bf6262
SangHyuk <107290583+SangHyuk7@users.noreply.github.com>
2024-04-07 16:59:56 +0900

    Time: 65 ms (52.43%), Space: 53 MB (53.55%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -1,14 +1,32 @@
-const hasCycle = function(head) {
-    if (!head) return false;
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
 
-    let slow = head;
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+//투포인터 사용해서 문제 해결
+//시간복잡도: O(N)
+
+const hasCycle = function(head) 
+{
     let fast = head;
+    let slow = head;
 
-    while (fast && fast.next) {
-        slow = slow.next;
+    while(fast && fast.next)
+    {
         fast = fast.next.next;
-        if (slow === fast) return true;
+        slow = slow.next;
+
+        if(fast === slow) {
+          return true
+        };
     }
-    
+
     return false;
 };
