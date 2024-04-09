a6834a6300eb00881e274daf90ad8501b7ca206f
SangHyuk <107290583+SangHyuk7@users.noreply.github.com>
2024-04-09 13:18:37 +0900

    Time: 67 ms (44.98%), Space: 54.5 MB (42.94%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -1,17 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+ 
+ // 마지막 노드 찾은후 0 부터 + 1 DFS 사용
+ // 시간복잡도 : O(n) 
+
 const isBalanced = function(root) {
-  const dfs = function(node) {
-    if (!node) {
-      return 0;
-    }
-    const left = 1 + dfs(node.left);
-    const right = 1 + dfs(node.right);
-    if (Math.abs(left - right) > 1) {
+    let balanced = true; 
 
-      return Infinity;
-    }
+    const dfs = function(node) {
+        if (!node) return 0;
 
-    return Math.max(left, right);
-  }
+        const left = dfs(node.left);
+        const right = dfs(node.right);
+
+        if (Math.abs(left - right) > 1) {
+            balanced = false;
+        }
+
+        return 1 + Math.max(left, right); 
+    }
     
-    return dfs(root) === Infinity ? false : true;
+    dfs(root);
+
+    return balanced;
+    
 };
