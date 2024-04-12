6eab9ac3b7307cef9041ad120e6f381a7d08ae80
SangHyuk <107290583+SangHyuk7@users.noreply.github.com>
2024-04-12 14:10:45 +0900

    Time: 86 ms (12.64%), Space: 58.9 MB (62.78%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -2,18 +2,22 @@
  * @param {number[]} prices
  * @return {number}
  */
-//시간복잡도 O(n)
-const maxProfit = function(prices) {
-    let minPrice = Infinity;
-    let maxProfitSoFar = 0;
+ //시간 복잡도: O(n)
+const maxProfit = function (prices) {
+  let buy = prices[0];
+  let sell = prices[0];
+  let profit = 0;
 
-    for (let i = 0; i < prices.length; i++) {
-        if (prices[i] < minPrice) {
-            minPrice = prices[i];
-        } else if (prices[i] - minPrice > maxProfitSoFar) {
-            maxProfitSoFar = prices[i] - minPrice;
-        }
+  for (let i = 0; i < prices.length; i++) {
+    if (i !== prices.length - 1 && prices[i] <= buy) {
+      sell = prices[i];
+      buy =  prices[i];
+    } else if (prices[i] > sell) {
+      sell = prices[i];
     }
 
-    return maxProfitSoFar;
+    profit = sell - buy > profit ? sell - buy : profit;
+  }
+
+  return profit;
 };
