ebca19c337e855eada7ba8f453bddb95c3666554
SangHyuk <107290583+SangHyuk7@users.noreply.github.com>
2024-04-15 17:23:07 +0900

    Time: 171 ms (81.69%), Space: 61.7 MB (92.45%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.js
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.js
@@ -3,25 +3,14 @@
  * @param {number} k
  * @return {number[][]}
  */
-const dist = (point) => {
-  return point[0] * point[0] + point[1] * point[1];
-}
+
+  // 유클리드 거리 계산법 사용 하지만 상대적 크기만을 비교 하기때문에 제곱 계산x
+  // 시간복잡도: O(n) 자바스크립트 엔진 .sort()는 퀵소트 사용 이므로 Olog(n)
+  // 다음 풀이에는 시간복잡도 개선을 위해서 직접 정렬 구현 
 
 const kClosest = function(points, k) {
-  const n = points.length;
-  let map = [];
+  const distance = (x, y) => x * x + y * y;
+  points.sort((a, b) => distance(a[0], a[1]) - distance(b[0], b[1]));
 
-  for (let i = 0; i < n; ++i) {
-    map.push({
-      distance: dist(points[i]),
-      points: points[i]
-    })
-  }
-  map.sort((a, b) => a.distance - b.distance);
-    
-  let ans = [];
-  for (let j = 0; j < k; j++) {
-      ans.push(map[j].points)
-  }
-  return ans;
+  return points.slice(0, k);
 };
