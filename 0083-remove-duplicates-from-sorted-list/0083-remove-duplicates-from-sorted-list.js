63e552ea5a81a29b48d2c8504ce41359103fcc48
SangHyuk <107290583+SangHyuk7@users.noreply.github.com>
2024-04-21 21:10:06 +0900

    Time: 70 ms (32.60%), Space: 52.5 MB (32.01%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+//모든 노드 순회 하므로 시간복잡도 : O(n)
+const deleteDuplicates = function(head) {
+  let current = head;
+  
+  while (current && current.next) {
+    if (current.val === current.next.val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  
+  return head;
+};
