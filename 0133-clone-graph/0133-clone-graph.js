60e1c1626a55701374105b605f20128392d6d9ed
SangHyuk <107290583+SangHyuk7@users.noreply.github.com>
2024-04-23 16:34:05 +0900

    Time: 61 ms (50.00%), Space: 51.8 MB (30.07%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.js
+++ 0133-clone-graph/0133-clone-graph.js
@@ -10,35 +10,29 @@
  * @param {Node} node
  * @return {Node}
  */
-
-// 알고리즘 이해부족 다시 풀어보기
-function Node(val, neighbors) {
-  this.val = val === undefined ? 0 : val;
-  this.neighbors = neighbors === undefined ? [] : neighbors;
-};
-
-
+//해쉬와 dfs 를 사용하여 문제 풀이
+//시간 복잡도: O(n + m) n: 노드, m은 노드의 간선
 const cloneGraph = function(node) {
-  if (node == null) {
-  return null;
+  if (!node) {
+    return null;
+  }
+
+  const map = new Map();
+
+  function dfs(node) {
+    if (map.has(node)) {
+      return map.get(node);
     }
 
-  let visited = new Map();
+    const newNode = new Node(node.val);
+    map.set(node, newNode);
 
-  let stack = [node];
+    for (const neighbor of node.neighbors) {
+      newNode.neighbors.push(dfs(neighbor));
+    }
 
-  visited.set(node, new Node(node.val, []));
-
-  while (stack.length > 0) {
-  let n = stack.pop();
-
-  for (let neighbor of n.neighbors) {
-    if (!visited.has(neighbor)) {
-      visited.set(neighbor, new Node(neighbor.val, []));
-                stack.push(neighbor)    }
-            visited.get(n).neighbors.push(visited.get(neighbor));
+    return newNode;
   }
-}
-
-    return visited.get(node);
+  
+  return dfs(node);
 };
