062f184400a5ae008e17f7cb2f1760c2e415dd55
SangHyuk <107290583+SangHyuk7@users.noreply.github.com>
2024-04-24 15:51:45 +0900

    Time: 55 ms (86.10%), Space: 50.8 MB (96.56%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -2,22 +2,34 @@
  * @param {string[]} tokens
  * @return {number}
  */
-
-const operators = {
-    '+': (a, b) => b + a,
-    '-': (a, b) => b - a,
-    '*': (a, b) => b * a,
-    '/': (a, b) => parseInt(b / a)
+//시간 복잡도: 모든 요소 순회 O(n);
+//FILO 스택구조 사용;
+const evalRPN = function(tokens) {
+  const stack = [];
+  
+  tokens.forEach(tokens => {
+    if (!isNaN(tokens)) {
+      stack.push(parseInt(tokens, 10));
+    } else {
+      const b = stack.pop();
+      const a = stack.pop();
+      
+      switch (tokens) {
+        case "+":
+          stack.push(a + b);
+          break;
+        case "-":
+          stack.push(a - b);
+          break;
+        case "*":
+          stack.push(a * b);
+          break;
+        case "/":
+          stack.push(Math.trunc(a / b));
+          break;
+      }
+    }
+  });
+  
+  return stack.pop();
 };
-
-/**
- * @param {string[]} tokens
- * @return {number}
- */
-
-//시간 복잡도 O(n)
-function evalRPN (tokens) {
-  const x = tokens.pop();
-
-  return operators[x] ? operators[x](evalRPN(tokens), evalRPN(tokens)) : parseInt(x);
-}
