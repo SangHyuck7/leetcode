d2e11c0b7243fc5566824d35675495478bce5616
SangHyuk <107290583+SangHyuk7@users.noreply.github.com>
2024-05-10 22:07:01 +0900

    Time: 56 ms (27.99%), Space: 48.8 MB (59.27%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -2,6 +2,7 @@
  * @param {string} s
  * @return {number}
  */
+//시간복잡도: O(n)
 const lengthOfLastWord = function(s) {
   const words = s.trim().split(' ');
   const lastWord = words[words.length - 1];
