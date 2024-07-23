ca8302f21923a78c0eaaad5fbf7b3774dc0c40f6
SangHyuck <107290583+SangHyuck7@users.noreply.github.com>
2024-07-23 19:33:16 +0900

    Time: 76 ms (35.94%), Space: 53 MB (14.24%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+ //시간복잡도: O(log(n))
+const isUgly = function(n) {
+  if (n <= 0) return false;
+    
+  const factors = [2, 3, 5];
+    
+  for (let factor of factors) {
+    while (n % factor === 0) {
+    n /= factor;
+    }
+  }
+    
+  return n === 1;
+};
