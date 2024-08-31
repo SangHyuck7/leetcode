81436977b222156eb20e10db322d2ec52d1b6355
SangHyuck <107290583+SangHyuck7@users.noreply.github.com>
2024-08-31 19:52:56 +0700

    Time: 51 ms (54.66%), Space: 49 MB (13.5%) - LeetHub

---
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+//풀이참고
+//n = 4 이상의 수에 대해서 (n-2)진수로 표현하면 항상 n이 “12”와 같은 형태가 되어 회문이 될 수 없기 때문에 항상 false
+const isStrictlyPalindromic = function(n) {
+  
+  return false;
+};
